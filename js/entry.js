fb17538f9fdc6878a8f276d5ee8cbeb728d7aa61
ahatiinii <ahatiinii.nv@gmail.com>
2023-12-16 09:13:34 -0700

    fix tables

--- js/entry.js
+++ js/entry.js
@@ -55,131 +55,41 @@ function add_entry (entry) {
 
     // first row (1.sg, 2.sg, 3.sg, 3a.sg)
     cell = sgrow.insertCell();
-    if (res[i].inflection.i_1_sg != undefined)
-        cell.innerHTML = res[i].inflection.i_1_sg;
+    if (entry.inflection.i_1_sg != undefined)
+        cell.innerHTML = entry.inflection.i_1_sg;
     cell = sgrow.insertCell();
-    if (res[i].inflection.i_2_sg != undefined)
-        cell.innerHTML = res[i].inflection.i_2_sg;
+    if (entry.inflection.i_2_sg != undefined)
+        cell.innerHTML = entry.inflection.i_2_sg;
     cell = sgrow.insertCell();
-    if (res[i].inflection.i_3_sg != undefined)
-        cell.innerHTML = res[i].inflection.i_3_sg;
+    if (entry.inflection.i_3_sg != undefined)
+        cell.innerHTML = entry.inflection.i_3_sg;
     cell = sgrow.insertCell();
-    if (res[i].inflection.i_3a_sg != undefined)
-        cell.innerHTML = res[i].inflection.i_3a_sg;
+    if (entry.inflection.i_3a_sg != undefined)
+        cell.innerHTML = entry.inflection.i_3a_sg;
 
     // second row (1.dpl, 2.dpl)
     cell = dplrow.insertCell();
-    if (res[i].inflection.i_1_dpl != undefined)
-        cell.innerHTML = res[i].inflection.i_1_dpl;
+    if (entry.inflection.i_1_dpl != undefined)
+        cell.innerHTML = entry.inflection.i_1_dpl;
     cell = dplrow.insertCell();
-    if (res[i].inflection.i_2_dpl != undefined)
-        cell.innerHTML = res[i].inflection.i_2_dpl;
+    if (entry.inflection.i_2_dpl != undefined)
+        cell.innerHTML = entry.inflection.i_2_dpl;
     cell = dplrow.insertCell();
     cell = dplrow.insertCell();
 
     // third row (1.pl, 2.pl, 3.pl, 3a.pl)
     cell = plrow.insertCell();
-    if (res[i].inflection.i_1_pl != undefined)
-        cell.innerHTML = res[i].inflection.i_1_pl;
+    if (entry.inflection.i_1_pl != undefined)
+        cell.innerHTML = entry.inflection.i_1_pl;
     cell = plrow.insertCell();
-    if (res[i].inflection.i_2_pl != undefined)
-        cell.innerHTML = res[i].inflection.i_2_pl;
+    if (entry.inflection.i_2_pl != undefined)
+        cell.innerHTML = entry.inflection.i_2_pl;
     cell = plrow.insertCell();
-    if (res[i].inflection.i_3_pl != undefined)
-        cell.innerHTML = res[i].inflection.i_3_pl;
+    if (entry.inflection.i_3_pl != undefined)
+        cell.innerHTML = entry.inflection.i_3_pl;
     cell = plrow.insertCell();
-    if (res[i].inflection.i_3a_pl != undefined)
-        cell.innerHTML = res[i].inflection.i_3a_pl;
+    if (entry.inflection.i_3a_pl != undefined)
+        cell.innerHTML = entry.inflection.i_3a_pl;
 
     document.body.appendChild(table);
 }
-
-
-
-// const accordion = document.getElementsByClassName('accordion');
-// for (let i = 0; i < accordion.length; ++i) {
-//     accordion[i].addEventListener('click', function () {
-//         this.classList.toggle('active');
-//         let panel = this.nextElementSibling;
-// 
-//         if (panel.style.display == 'block') {
-//             panel.style.display = 'none';
-//         }
-//         else {
-//             panel.style.display = 'block';
-//         }
-//     });
-// }
-// 
-// 
-// show_entry(results[0]);
-
-
-// function get_word (word) {
-//     const table = document.createElement('table');
-// 
-//     const index = this.store.index('by_word');
-//     const request = index.get(word);
-// 
-//     request.onerror = function () {
-//         console.log('Could not access table');
-//     }
-// 
-//     request.onsuccess = function () {
-//         const matching = request.result;
-//         if (matching !== undefined) {
-//             document.getElementById("word").innerHTML += "<b>" + matching.word + "</b>";
-//             document.getElementById("root").innerHTML += "root: " + matching.root;
-// 
-//             for (let key in matching.morphemes) {
-//                 document.getElementById("morphology").innerHTML += matching.morphemes[key] + " (" + key + ") ";
-//             }
-// 
-//             document.getElementById("definition").innerHTML += "\"" + matching.definition + "\"";
-// 
-//             var next = 0;
-// 
-//             const row = table.insertRow();
-// 
-//             var cell = row.insertCell();
-//             cell.appendChild(document.createTextNode(""));
-//             cell = row.insertCell();
-//             cell.appendChild(document.createTextNode("first"));
-//             cell = row.insertCell();
-//             cell.appendChild(document.createTextNode("second"));
-//             cell = row.insertCell();
-//             cell.appendChild(document.createTextNode("third"));
-//             cell = row.insertCell();
-//             cell.appendChild(document.createTextNode("fourth"));
-// 
-//             for (let i = 0; i < 3; ++i) {
-//                 const row = table.insertRow();
-// 
-//                 const cell = row.insertCell();
-// 
-//                 if (i == 0) {
-//                     cell.appendChild(document.createTextNode("singular"));
-//                 }
-//                 else if (i == 1) {
-//                     cell.appendChild(document.createTextNode("duo-plural"));
-//                 }
-//                 else {
-//                     cell.appendChild(document.createTextNode("plural"));
-//                 }
-// 
-//                 for (let j = 0; j < 4; ++j) {
-//                     const cell = row.insertCell();
-//                     if (!(i == 1 && (j == 2 || j == 3))) {
-//                         cell.appendChild(document.createTextNode(Object.values(matching.inflection)[next]));
-//                         ++next;
-//                     }
-//                     cell.style.border = '1px solid black';
-//                 }
-//             }
-// 
-//             document.body.appendChild(table);
-//         }
-//         else {
-//             console.log('Could not find word');
-//         }
-//     }
